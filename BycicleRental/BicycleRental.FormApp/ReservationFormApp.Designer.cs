cd0ab9ed5fe5b67a12d4ca603f35ba871b8d9ff7
LMilenov <lusi_milenov@abv.bg>
2023-08-29 01:58:35 +0300

    Ended the project by adding all the form apps.

--- BycicleRental/BicycleRental.FormApp/ReservationFormApp.Designer.cs
+++ BycicleRental/BicycleRental.FormApp/ReservationFormApp.Designer.cs
@@ -28,20 +28,171 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(196, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Find by ID";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(18, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 64);
+            this.listBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Create reservation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Start date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "End date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Customer ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 322);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Bicycle ID";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(93, 223);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(93, 257);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker2.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 289);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 23);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 322);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(200, 23);
+            this.textBox3.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(287, 59);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Add reservation";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // ReservationFormApp
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(316, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
             this.Name = "ReservationFormApp";
             this.Text = "ReservationFormApp";
             this.Load += new System.EventHandler(this.ReservationFormApp_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
--- BycicleRental/BicycleRental.FormApp/ReservationFormApp.cs
+++ BycicleRental/BicycleRental.FormApp/ReservationFormApp.cs
@@ -4,22 +4,105 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace BicycleRental.FormApp
 {
     public partial class ReservationFormApp : Form
     {
+        private List<Reservation> reservations; 
+        private List<Customer> customers;      
+        private List<Bycicle> bicycles;
         public ReservationFormApp()
         {
             InitializeComponent();
+            reservations = new List<Reservation>();
+            customers = new List<Customer>();
+            bicycles = new List<Bycicle>();
+            reservations = new List<Reservation>
+            {
+                new Reservation { Id = 1, ReservationStart = DateTime.Now, ReservationEnd = DateTime.Now.AddDays(3) },
+                new Reservation { Id = 2, ReservationStart = DateTime.Now, ReservationEnd = DateTime.Now.AddDays(2) },
+            };
         }
 
         private void ReservationFormApp_Load(object sender, EventArgs e)
         {
-            
+
         }
-        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int searchId))
+            {
+                Reservation foundReservation = reservations.Find(reservation => reservation.Id == searchId);
+
+                if (foundReservation != null)
+                {
+                    DisplayReservation(foundReservation);
+                }
+                else
+                {
+                    MessageBox.Show("Reservation not found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ClearReservationDisplay();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid ID format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ClearReservationDisplay();
+            }
+        }
+        private void DisplayReservation(Reservation reservation)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add($"ID: {reservation.Id}");
+            listBox1.Items.Add($"Start Date: {reservation.ReservationStart}");
+            listBox1.Items.Add($"End Date: {reservation.ReservationEnd}");
+        }
+
+        private void ClearReservationDisplay()
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out int customerId) &&
+        int.TryParse(textBox3.Text, out int bicycleId))
+            {
+                Customer customer = customers.FirstOrDefault(c => c.Id == customerId);
+                Bycicle bicycle = bicycles.FirstOrDefault(b => b.Id == bicycleId);
+
+                if (customer != null && bicycle != null)
+                {
+                    DateTime startDate = dateTimePicker1.Value;
+                    DateTime endDate = dateTimePicker2.Value;
+
+                    Reservation newReservation = new Reservation
+                    {
+                        ReservationStart = startDate,
+                        ReservationEnd = endDate,
+                        Customer = customer,
+                        Bycicle = bicycle,
+                    };
+
+                    // Assuming reservations is a list that holds reservations
+                    reservations.Add(newReservation);
+                    MessageBox.Show("Reservation created successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Customer or Bicycle not found. Please enter valid IDs.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid ID format. Please enter valid numbers.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
